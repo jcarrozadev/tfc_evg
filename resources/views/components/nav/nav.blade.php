895543e5137d7eae71cd1a0bf4fa8f150322fa2c
zmartinl <zmartinllera@gmail.com>
2025-04-06 01:48:29 +0200

    Added Nav

---
+++ resources/views/components/nav/nav.blade.php
@@ -0,0 +1,26 @@
+<nav class="d-flex flex-row align-items-start p-3" style="width: 100%; height: auto;">
+    <div class="d-flex flex-row align-items-center me-4">
+        <img src="{{ asset('img/logo.png') }}" alt="Logo" class="img-fluid me-3" style="max-width: 120px;">
+
+        <ul class="nav nav-pills flex-row">
+            <li class="nav-item">
+                <a class="nav-link" href="">Inicio</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="">Libro Guardias</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="">Guardias</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="">Profesores</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="">Cursos</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="">Clases</a>
+            </li>
+        </ul>
+    </div>
+</nav>
