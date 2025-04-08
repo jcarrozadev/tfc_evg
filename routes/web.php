2f1db939f14a2ceac227e1432013fb77bd259c91
zmartinl <zmartinllera@gmail.com>
2025-04-09 00:20:28 +0200

    Modify NavStyles & viewAdmin

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('test');
-});
 
-Route::get('/login', function () {
+Route::get('/', function () {
     return view('login.login');
 });
 
