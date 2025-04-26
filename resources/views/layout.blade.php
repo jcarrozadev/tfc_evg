0f2e7007b43ff0035372f62f77ced5d30be17a1a
zmartinl <zmartinllera@gmail.com>
2025-04-26 19:22:43 +0200

    Added favicon

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,6 +7,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="author" content="Javier Arias Carroza | Zeus Martin Llera">
         <title>@yield('title')</title>
+        <link rel="icon" type="image/png" href="{{ asset('favicon.ico') }}">
         <link rel="stylesheet" href="{{ asset('css/common.css') }}">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
