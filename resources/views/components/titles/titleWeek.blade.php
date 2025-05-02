bf938c93675444cabdc8e91d9ec9d25d8125f9d4
jcarrozadev <jariasc14@gmail.com>
2025-05-02 16:27:51 +0200

    Restyled datatables

--- resources/views/components/titles/titleWeek.blade.php
+++ resources/views/components/titles/titleWeek.blade.php
@@ -14,6 +14,6 @@
     $rango_fechas_semanal = "$lunes_formateado - $viernes_formateado";
 ?>
 
-<div class="text-white p-3 m-4 rounded bg-custom">
-    <h1 class="ms-5 fs-2 fw-bold">{{ $title }} - {{ $rango_fechas_semanal }}</h1>
+<div class="text-white p-3 px-3 mb-3 bg-custom rounded">
+    <h1 class="fw-bold fs-3">{{ $title }} - {{ $rango_fechas_semanal }}</h1>
 </div>
